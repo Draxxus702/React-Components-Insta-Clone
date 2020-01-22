21b2e10ba8c3e5779874501fe2e061543873f5ce
Draxxus702 <58435416+KennithHowe2@users.noreply.github.com>
2020-01-21 17:04:45 -0800

    props on props on props

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,11 +3,20 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
+
+
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  console.log('PostPageprops', props)
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.postData.map(p => (
+        
+<Post 
+key={p.thumbnailUrl}
+post={p}
+/>
+      ))}
     </div>
   );
 };
